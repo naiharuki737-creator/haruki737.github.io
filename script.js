889155f7872e40a4f8b2fdbc81a1d528af2e7fee
naiharuki737-creator <naiharuki737@gmail.com>
2026-07-07 20:18:24 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 const canvas=document.getElementById("game");
 const ctx=canvas.getContext("2d");
 
-const eatSound=document.getElementById("eatSound");
-const bgm=document.getElementById("bgm");
-
 let snake;
 let food;
 let dx;
@@ -11,15 +8,34 @@ let dy;
 let score;
 let timer;
 
+let audioCtx;
+
+
+function sound(freq,time){
+
+if(!audioCtx){
+audioCtx=new AudioContext();
+}
+
+let osc=audioCtx.createOscillator();
+
+osc.frequency.value=freq;
+osc.type="square";
+
+osc.connect(audioCtx.destination);
+
+osc.start();
+
+osc.stop(audioCtx.currentTime+time);
+
+}
+
+
 
 function startGame(){
 
 document.getElementById("start").style.display="none";
 canvas.style.display="block";
-
-
-bgm.volume=0.2;
-bgm.play();
 
 
 snake=[
@@ -43,8 +59,13 @@ document.getElementById("score").innerHTML=0;
 
 let speed=Number(document.getElementById("level").value);
 
+
 clearInterval(timer);
+
 timer=setInterval(loop,speed);
+
+
+sound(500,0.1);
 
 }
 
@@ -56,10 +77,12 @@ ctx.fillStyle="black";
 ctx.fillRect(0,0,400,400);
 
 
+
 let head={
 x:snake[0].x+dx,
 y:snake[0].y+dy
 };
+
 
 
 if(
@@ -68,19 +91,25 @@ head.y<0||
 head.x>=400||
 head.y>=400
 ){
-end();
+
+gameOver();
 return;
+
 }
+
 
 
 for(let s of snake){
 
 if(head.x==s.x&&head.y==s.y){
-end();
+
+gameOver();
 return;
+
 }
 
 }
+
 
 
 snake.unshift(head);
@@ -94,8 +123,8 @@ score++;
 document.getElementById("score").innerHTML=score;
 
 
-eatSound.currentTime=0;
-eatSound.play();
+sound(900,0.15);
+
 
 
 food={
@@ -115,11 +144,14 @@ snake.pop();
 ctx.fillStyle="lime";
 
 snake.forEach(s=>{
+
 ctx.fillRect(s.x,s.y,18,18);
+
 });
 
 
 ctx.fillStyle="red";
+
 ctx.fillRect(food.x,food.y,18,18);
 
 }
@@ -128,43 +160,52 @@ ctx.fillRect(food.x,food.y,18,18);
 
 document.addEventListener("keydown",e=>{
 
+
 if(e.key=="ArrowUp"&&dy==0){
+
 dx=0;
 dy=-20;
+
 }
+
 
 if(e.key=="ArrowDown"&&dy==0){
+
 dx=0;
 dy=20;
+
 }
+
 
 if(e.key=="ArrowLeft"&&dx==0){
+
 dx=-20;
 dy=0;
+
 }
 
+
 if(e.key=="ArrowRight"&&dx==0){
+
 dx=20;
 dy=0;
+
 }
+
 
 });
 
 
 
-function end(){
+function gameOver(){
 
 clearInterval(timer);
 
-let best=localStorage.getItem("best")||0;
-
-
-if(score>best){
-localStorage.setItem("best",score);
-}
+sound(200,0.3);
 
 
 alert("ゲームオーバー！\nスコア："+score);
+
 
 location.reload();
 
