3b9532ed46abc06a8ed2b370571dbac1f65bab5e
naiharuki737-creator <naiharuki737@gmail.com>
2026-07-07 20:16:08 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-const canvas = document.getElementById("game");
-const ctx = canvas.getContext("2d");
+const canvas=document.getElementById("game");
+const ctx=canvas.getContext("2d");
+
+const eatSound=document.getElementById("eatSound");
+const bgm=document.getElementById("bgm");
 
 let snake;
 let food;
@@ -7,7 +10,6 @@ let dx;
 let dy;
 let score;
 let timer;
-let speed;
 
 
 function startGame(){
@@ -15,82 +17,92 @@ function startGame(){
 document.getElementById("start").style.display="none";
 canvas.style.display="block";
 
-speed = Number(document.getElementById("level").value);
+
+bgm.volume=0.2;
+bgm.play();
+
 
 snake=[
 {x:200,y:200}
 ];
+
 
 food={
 x:Math.floor(Math.random()*20)*20,
 y:Math.floor(Math.random()*20)*20
 };
 
+
 dx=20;
 dy=0;
 score=0;
 
-document.getElementById("score").innerHTML=score;
+
+document.getElementById("score").innerHTML=0;
+
+
+let speed=Number(document.getElementById("level").value);
 
 clearInterval(timer);
-timer=setInterval(gameLoop,speed);
+timer=setInterval(loop,speed);
 
 }
 
 
 
-function gameLoop(){
+function loop(){
 
 ctx.fillStyle="black";
 ctx.fillRect(0,0,400,400);
 
 
-// ヘビ移動
 let head={
 x:snake[0].x+dx,
 y:snake[0].y+dy
 };
 
 
-// 壁に当たる
 if(
-head.x<0 ||
-head.y<0 ||
-head.x>=400 ||
+head.x<0||
+head.y<0||
+head.x>=400||
 head.y>=400
 ){
-gameOver();
+end();
 return;
 }
 
 
-// 自分に当たる
-for(let i=0;i<snake.length;i++){
-if(head.x==snake[i].x &&
-head.y==snake[i].y){
-gameOver();
+for(let s of snake){
+
+if(head.x==s.x&&head.y==s.y){
+end();
 return;
 }
+
 }
 
 
 snake.unshift(head);
 
 
-// 食べた
-if(
-head.x==food.x &&
-head.y==food.y
-){
+
+if(head.x==food.x&&head.y==food.y){
 
 score++;
 
 document.getElementById("score").innerHTML=score;
 
+
+eatSound.currentTime=0;
+eatSound.play();
+
+
 food={
 x:Math.floor(Math.random()*20)*20,
 y:Math.floor(Math.random()*20)*20
 };
+
 
 }else{
 
@@ -99,12 +111,11 @@ snake.pop();
 }
 
 
-// 描画
 
 ctx.fillStyle="lime";
 
-snake.forEach(part=>{
-ctx.fillRect(part.x,part.y,18,18);
+snake.forEach(s=>{
+ctx.fillRect(s.x,s.y,18,18);
 });
 
 
@@ -115,26 +126,24 @@ ctx.fillRect(food.x,food.y,18,18);
 
 
 
-// 操作
+document.addEventListener("keydown",e=>{
 
-document.addEventListener("keydown",function(e){
-
-if(e.key=="ArrowUp" && dy==0){
+if(e.key=="ArrowUp"&&dy==0){
 dx=0;
 dy=-20;
 }
 
-if(e.key=="ArrowDown" && dy==0){
+if(e.key=="ArrowDown"&&dy==0){
 dx=0;
 dy=20;
 }
 
-if(e.key=="ArrowLeft" && dx==0){
+if(e.key=="ArrowLeft"&&dx==0){
 dx=-20;
 dy=0;
 }
 
-if(e.key=="ArrowRight" && dx==0){
+if(e.key=="ArrowRight"&&dx==0){
 dx=20;
 dy=0;
 }
@@ -143,24 +152,19 @@ dy=0;
 
 
 
-// ゲーム終了
-
-function gameOver(){
+function end(){
 
 clearInterval(timer);
 
-let best=localStorage.getItem("best") || 0;
+let best=localStorage.getItem("best")||0;
+
 
 if(score>best){
-
-best=score;
 localStorage.setItem("best",score);
-
 }
 
-document.getElementById("best").innerHTML=best;
 
-alert("ゲームオーバー！ スコア："+score);
+alert("ゲームオーバー！\nスコア："+score);
 
 location.reload();
 
